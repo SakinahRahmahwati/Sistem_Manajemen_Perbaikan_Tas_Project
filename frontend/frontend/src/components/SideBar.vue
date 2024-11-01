8c154a6a5b31c8cbc594ddde4509931822e42eb3
SakinahRahmahwati <sakinahrahmawati08@gmail.com>
2024-11-02 05:34:08 +0700

    FE1

--- frontend/frontend/src/components/SideBar.vue
+++ frontend/frontend/src/components/SideBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="sidebar">
+    <div class="sidebar" data-color="white">
         <!--
          Tip 1: You can change the color of the sidebar using: data-color="purple | blue | green | orange | red"
  
@@ -7,57 +7,57 @@
      -->
         <div class="sidebar-wrapper">
             <div class="logo">
-                <a href="http://www.creative-tim.com" class="simple-text">
+                <a href="#" class="simple-text">
                     Dian Tas
                 </a>
             </div>
             <ul class="nav">
                 <li class="nav-item active">
                     <a class="nav-link" href="dashboard.html">
-                        <i class="nc-icon nc-chart-pie-35"></i>
+                        <i class="bi bi-house-door-fill"></i>
                         <p>Dashboard</p>
                     </a>
                 </li>
                 <li>
                     <a class="nav-link" href="./user.html">
-                        <i class="nc-icon nc-circle-09"></i>
-                        <p>User Profile</p>
+                        <i class="bi bi-briefcase-fill"></i>
+                        <p>Perbaikan Tas</p>
                     </a>
                 </li>
                 <li>
                     <a class="nav-link" href="./table.html">
-                        <i class="nc-icon nc-notes"></i>
-                        <p>Table List</p>
+                        <i class="bi bi-gear"></i>
+                        <p>Jenis Layanan</p>
                     </a>
                 </li>
                 <li>
                     <a class="nav-link" href="./typography.html">
-                        <i class="nc-icon nc-paper-2"></i>
-                        <p>Typography</p>
+                        <i class="bi bi-list-ul"></i>
+                        <p>Material</p>
                     </a>
                 </li>
                 <li>
                     <a class="nav-link" href="./icons.html">
-                        <i class="nc-icon nc-atom"></i>
-                        <p>Icons</p>
+                        <i class="bi bi-people-fill"></i>
+                        <p>Pelanggan</p>
                     </a>
                 </li>
                 <li>
                     <a class="nav-link" href="./maps.html">
-                        <i class="nc-icon nc-pin-3"></i>
-                        <p>Maps</p>
+                        <i class="bi bi-boxes"></i>
+                        <p>Pemasok</p>
                     </a>
                 </li>
                 <li>
                     <a class="nav-link" href="./notifications.html">
-                        <i class="nc-icon nc-bell-55"></i>
-                        <p>Notifications</p>
+                        <i class="bi bi-cash-coin"></i>
+                        <p>Laporan Keuangan</p>
                     </a>
                 </li>
-                <li class="nav-item active active-pro">
-                    <a class="nav-link active" href="upgrade.html">
-                        <i class="nc-icon nc-alien-33"></i>
-                        <p>Upgrade to PRO</p>
+                <li>
+                    <a class="nav-link" href="./icons.html">
+                        <i class="bi bi-person-fill"></i>
+                        <p>Akun</p>
                     </a>
                 </li>
             </ul>
